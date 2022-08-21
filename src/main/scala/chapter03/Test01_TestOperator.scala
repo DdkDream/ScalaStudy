002917ae93b8cb19c4dc33e560c652b95b8acf1e
zrm <643910751@qq.com>
2022-08-21 22:34:57 +0800

    运算符练习

---
+++ src/main/scala/chapter03/Test01_TestOperator.scala
@@ -0,0 +1,16 @@
+package chapter03
+
+object Test01_TestOperator {
+  def main(args: Array[String]): Unit = {
+    // 1.算术运算符
+    val result1 = 10 / 3
+    println(result1)
+
+    val result2: Double = 10 / 3
+    println(result2)
+
+    val result3: Double = 10.0 / 3
+    println(result3)
+  }
+
+}
