0d193f4fd13f62a794f3e0b56c11ef6d73b421de
zrm <643910751@qq.com>
2022-08-21 22:29:56 +0800

    强制类型转换，数据溢出

--- src/main/scala/chapter02/Test08_DataTypeConversion.scala
+++ src/main/scala/chapter02/Test08_DataTypeConversion.scala
@@ -2,6 +2,8 @@ package chapter02
 
 object Test08_DataTypeConversion {
   def main(args: Array[String]): Unit = {
+    // 1、自动转换
+
     val a1: Byte = 10
     val b1: Long = 2353L
     val result1: Long = a1 + b1
@@ -24,6 +26,27 @@ object Test08_DataTypeConversion {
     val result4: Int = a4 + b4
     val result44: Int = a4 + b4 + c4
     println(result4)
+
+    // 2、强制类型转换
+    val n1: Int = 2.5.toInt
+    println("n1:" + n1)
+
+    val n2: Int = 2.6.toInt + 3.7.toInt
+    val n3: Int = (2.6 + 3.7).toInt
+    println("n2: " + n2)
+    println("n3: " + n3)
+
+
+    // 3、数值类型和String类型的转换
+    val n: Int = 27
+    val s: String = n + ""
+    println(s)
+
+    val m: Int = "12".toInt
+    val f: Float = "12.3".toFloat
+    val f2: Int = "12.3".toInt
+    println(f2)
+
 
   }
 
